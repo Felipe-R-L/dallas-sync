2bbe30f5547a987fa633142d883869aa8c827e3e
Felipe-R-L <leone.feliper@gmail.com>
2025-07-22 22:09:11 -0300

    refactor: move prisma folder to infra

--- backend/src/infra/database/prisma/prisma.service.ts
+++ backend/src/infra/database/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '../../../../generated/prisma/client'
 
 @Injectable()
 export class PrismaService
