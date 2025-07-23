3d1cabca6b7b44ab7ec280988a48f972d95532a0
Felipe-R-L <leone.feliper@gmail.com>
2025-07-22 22:42:01 -0300

    feat: add delete method to room-repository

--- backend/src/domain/repositories/room-repository.ts
+++ backend/src/domain/repositories/room-repository.ts
@@ -5,4 +5,5 @@ export abstract class RoomRepository {
   abstract findById(id: string): Promise<Room | null>
   abstract findAll(): Promise<Room[]>
   abstract update(room: Room): Promise<void>
+  abstract delete(id: string): Promise<void>
 }
